cb5cd766281723ccf3ef7e71ed6c0da17eab0746
teidesu <me@tei.su>
2020-05-28 01:35:44 +0300

    new(parsers): added AniFilm Sibnet parser
    Average efficiency: 0.80.
    https://video.sibnet.ru/users/AniFilmTV

---
+++ src/importers/sibnet/anifilm.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'AniFilmTV',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(\d+) - (.+?)(?:\.{3})?(?: \/ (.+?))? \| anifilm/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[2], m[3]],
+                    part: m => m[1],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: 'AniFilm'
+                }
+            }
+        ])
+    })
+}

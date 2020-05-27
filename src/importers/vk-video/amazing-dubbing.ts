459f7ab4a38328ee85e00f89884ad951ffac439b
teidesu <me@tei.su>
2020-05-27 14:38:19 +0300

    new(parsers): Added amazing dubbing VK parser.
    Average efficiency is about 0.5, mostly because they also translate dramas.
    https://vk.com/videos-126822319

---
+++ src/importers/vk-video/amazing-dubbing.ts
@@ -0,0 +1,33 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/vk-video', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/vk-video']({
+        owner: -126822319,
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(?:\[\d+\+\] ?)?(Озвучка |\[Субтитры\] )?(\d+) серия (?:MVO ?|END ?)?\| ([^|]+?) (?:\| .+)* ?\[Amazing Dubbing\]$/,
+                target: v => v.title,
+                fields: {
+                    target: m => m[3],
+                    part: m => m[2],
+                    kind: m => m[1] === '[Субтитры] ' ? 'sub' : 'dub',
+                    lang: 'ru',
+                    hq: true,
+                    author: 'Amazing Dubbing'
+                },
+                moreUrls: [
+                    function (v) {
+                        let m
+                        if ((m = v.description.match(/^Ссылка на скачивание: https?:\/\/(?:www\.)?myvi\.top\/[a-zA-Z0-9]+\?v=(.+)$/m))) {
+                            return ['https://www.myvi.top/embed/' + m[1]]
+                        }
+
+                        return []
+                    }
+                ]
+            }
+        ])
+    })
+}

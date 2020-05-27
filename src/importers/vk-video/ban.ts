6950caabb225dffa9d5b113b95a477f550b787a0
teidesu <me@tei.su>
2020-05-27 22:09:11 +0300

    new(parsers): added BAN Official VK parser.
    Average efficiency: 0.65, mostly because of non-anime videos like live streams.
    https://vk.com/videos-125588597

---
+++ src/importers/vk-video/ban.ts
@@ -0,0 +1,33 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/vk-video', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/vk-video']({
+        owner: -125588597,
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(?:\[Озвучка\] )?\[(?:озвучка \| )?(\d+|[фФ]ильм)(?: серия)?(?: \| movie)?(?: *\(\d+\))?\] (.+?)(?: [\/|] (.+?))? (?:\[(?:\d+ [сСcC]ерия|MOVIE)\] (?:\[END\] )?\[(.+?)\]|\| (?:русская озвучка|by) (.+?)(?: \|.+)?)$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[2], m[3]],
+                    part: m => m[1].match(/фильм/i) ? 1 : m[1],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[4] || m[5] || 'Ban'
+                }
+            },
+            {
+                regex: /^(\[.+?\] )?(.+?)(?: \/ (.+?))?(?: -)? (?:\(ban\) )?(?:[\[(])?(\d+)(?: из \d+\]|\))?(?: русская озвучка)?(?: серия)?(?: END)?(?: *\(рус\.озв\.Ban\)| *[\(\[](?:Озвучка(?: -)? )?(.+?)[\)\]])?(?: \[animaunt\.ru\]| \| BAN \|)?$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[2], m[3]],
+                    part: m => m[4],
+                    kind: m => m[1]?.match(/субтитры/i) ? 'sub' : 'dub',
+                    lang: 'ru',
+                    author: m => m[5] || 'Ban'
+                }
+            }
+        ])
+    })
+}

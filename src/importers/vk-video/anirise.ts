d729fa726a2bba72acc7cca93bd5ee91aa4bea05
teidesu <me@tei.su>
2020-05-27 21:36:54 +0300

    new(parsers): added AniRise VK parser.
    Average efficiency: 0.73.
    https://vk.com/videos-151876221

---
+++ src/importers/vk-video/anirise.ts
@@ -0,0 +1,50 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/vk-video', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/vk-video']({
+        owner: -151876221,
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^\[(Озвучка )?anirise\] *(.+?) (\d+)(?: END)? серия(?:(?: \/)? (.+?))?(?: \((.+?)\)|$)$/i,
+                target: v => v.title,
+                skip: v => !!v.title.match(/трейлер/i),
+                fields: {
+                    target: m => [m[2], m[4]],
+                    part: m => m[3],
+                    kind: m => m[1] || m[5]?.match(/озвучка/i) ? 'dub' : 'sub',
+                    lang: 'ru',
+                    author: m => m[1] ? m[5]?.match(/озвучка/i) ? 'AniRise' : m[5] : 'AniRise'
+                }
+            },
+            {
+                regex: /^(.+?) (\d+)(?: *\(\d+\))? серия \/ (.+?) (?:\| (.+?) \[|\([оО]звучка anirise\)|\[)/i,
+                target: v => v.title,
+                skip: v => !!v.title.match(/трейлер/i),
+                fields: {
+                    target: m => [m[1], m[3]],
+                    part: m => m[2],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[4] || 'AniRise'
+                }
+            },
+            {
+                regex: /^(.+?) (\d+) (OVA|серия)(?: \/ (.+?))? (?:\([pPрР]усская озвучка (.+?)\) (.+?)|\| [pPрР]усская озвучка (.+?))(?: \[anirise\]|$)/i,
+                target: v => v.title,
+                skip: v => !!v.title.match(/трейлер/i),
+                fields: {
+                    target: m => [
+                        m[1] + (m[3] === 'OVA' ? ' OVA' : ''),
+                        m[6] + (m[3] === 'OVA' ? ' OVA' : '')
+                    ],
+                    part: m => m[2],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[5] || m[7] || 'AniRise'
+                }
+            }
+        ])
+    })
+}

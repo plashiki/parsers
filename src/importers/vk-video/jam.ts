838dc103ab5a60d938a5465414c0039f707c252e
teidesu <me@tei.su>
2020-05-27 22:46:34 +0300

    new(parsers): added JAM Club VK parser
    Average efficiency: 0.9
    https://vk.com/videos-34484100

---
+++ src/importers/vk-video/jam.ts
@@ -0,0 +1,23 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/vk-video', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/vk-video']({
+        owner: -34484100,
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(.+?)(?: \/ (.+?))? \[(\d+)(?: *\(\d+\))? из (?:\d+\+?(?: *\(\d+\))?|[хХxX]{3})\](?: Озвучка (.+?))? \[© JAM CLUB\]$/i,
+                target: v => v.title,
+                skip: v => !!v.title.match(/amv/i),
+                fields: {
+                    target: m => [m[1], m[2]],
+                    part: m => m[3],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[4] || 'Jam'
+                }
+            }
+        ])
+    })
+}

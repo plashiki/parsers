3b797c70245c179c12b6b279ce093b85efcd9ae3
teidesu <me@tei.su>
2020-05-28 00:30:51 +0300

    new(parsers): added MedusaSub VK parser
    Average efficiency: 0.97
    https://vk.com/videos-158435497

---
+++ src/importers/vk-video/medusasub.ts
@@ -0,0 +1,23 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/vk-video', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/vk-video']({
+        owner: -158435497,
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^\[?MedusaSub\] (.+)? [|\-–]? (.+) [-–] (\d+)(?: END)? серия [-–] русские субтитры$/i,
+                target: v => v.title,
+                skip: v => !!v.title.match(/amv/i),
+                fields: {
+                    target: m => [m[1], m[2]],
+                    part: m => m[3],
+                    kind: 'sub',
+                    lang: 'ru',
+                    author: 'MedusaSub'
+                }
+            }
+        ])
+    })
+}

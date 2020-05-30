cb66711ae96f9934e3747c386701da582c6a8463
teidesu <me@tei.su>
2020-05-30 18:57:59 +0300

    new(parsers): added AniRaccoon Myvi parser
    Average efficiency: 0.88, mostly because of non-anime videos
    https://myvi.top/id1p7o

---
+++ src/importers/myvi/aniraccoon.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/myvi', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/myvi']({
+        owner: 'id1p7o',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^\[(sub|dub)\] (.+) \/ (.+) \/ (\d+) серия$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[2], m[3]],
+                    part: m => m[4],
+                    kind: m => m[1],
+                    lang: 'ru',
+                    author: 'AniRaccoon'
+                }
+            }
+        ])
+    })
+}

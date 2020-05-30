c8afdf69c9f3e6dbe6e06a4baed42477fd1d51d5
teidesu <me@tei.su>
2020-05-30 15:31:36 +0300

    new(parsers): added Таканаши Sibnet parser
    Average efficiency: N/A, they have A LOT of videos. Old ones don't have common format, new ones seem to be parsed with a decent success rate.
    https://video.sibnet.ru/users/Таканаши

---
+++ src/importers/sibnet/takanashi.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'Таканаши',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(\d+)(?: *\(\d+\))?(?: END)? серия (.+?) [-\/] (.+?)(?: [-\/] (.+?))? русская озвучка (.*?)$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[2], m[3], m[4]],
+                    part: m => m[1],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[5] === 'MVO' ? '' : m[5]
+                }
+            }
+        ])
+    })
+}

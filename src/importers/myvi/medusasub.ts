a1eb3b1729d8488ce6222f48fed4ffb786dd7f44
teidesu <me@tei.su>
2020-05-30 19:38:11 +0300

    new(parsers): added MedusaSub Myvi parser
    Average efficiency: 0.97.
    https://myvi.top/idr6bo

---
+++ src/importers/myvi/medusasub.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/myvi', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/myvi']({
+        owner: 'idr6bo',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^\[MedusaSub\] (.+?)(?: \([A-Z]{3}\))? [-–] (\d+)(?: END)?( OVA)?/i,
+                target: v => v.title,
+                fields: {
+                    target: m => m[1] + (m[3] || ''),
+                    part: m => m[2],
+                    kind: 'sub',
+                    lang: 'ru',
+                    author: 'MedusaSub'
+                }
+            }
+        ])
+    })
+}

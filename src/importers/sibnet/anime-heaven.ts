d82dcaafb69361ea7f34289d8c35586bb042b908
teidesu <me@tei.su>
2020-05-30 13:21:53 +0300

    new(parsers): added Anime Heaven Sibnet parser
    Average efficiency: N/A, they have A LOT of translations. Parser seems to be working with oldest ones and newest ones though.
    https://video.sibnet.ru/users/Anime_Heaven

---
+++ src/importers/sibnet/anime-heaven.ts
@@ -0,0 +1,26 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'Anime_Heaven',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(.+?) \/ (.+?)(?:\.{3})?(?: \/ (.+?)(?:\.{3})?)?(?: -)?(?: (\d+) сезон)? (\d+) серия(?: END)?(?: \[\d+\])?(?: END)? \((озвучка|субтитры)\)(?: \[(.+?)\])?(?: \[\d+\])?$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [
+                        m[1],
+                        m[2] + (m[4] ? ' ' + m[4] : ''),
+                        m[3]
+                    ],
+                    part: m => m[5],
+                    kind: m => m[6].match(/субтитры/i) ? 'sub' : 'dub',
+                    lang: 'ru',
+                    author: m => m[7]
+                }
+            }
+        ])
+    })
+}

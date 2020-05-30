12e7d4dc539d27fba4ed31fd5ee9970f4dd26789
teidesu <me@tei.su>
2020-05-30 13:56:52 +0300

    new(parsers): added Bucherino Sibnet parser
    Average efficiency: N/A, they have A LOT of translations. Parser seems to be working with oldest ones and newest ones though.
    https://video.sibnet.ru/users/bucherino

---
+++ src/importers/sibnet/bucherino.ts
@@ -0,0 +1,37 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'bucherino',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                // look at dis duude
+                regex: /^(.+?)(?: \/ (.+?))?(?: *(?:\[(?:\d+(?: (?:и(?:з(?: (?:\d+\+?(?:\])?)?)?)?)?)?)?)?)?(?:\S*\?)? - (\d+) серия/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[1], m[2]],
+                    part: m => m[3],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: ''
+                }
+            },
+            {
+                regex: /^(?:\[(.+?)\] )?(.+?) [\/|] (.+?) (\[(\d+)\]|\((\d+) [cс]ерия\))(?: \[(.+?)\])?/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [
+                        m[2].replace(/(\S)_(\S)/g, (_, $1, $2) => $1 + ' ' + $2),
+                        m[3]
+                    ],
+                    part: m => m[5] || m[6],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => (m[1] || '') + (m[7] ? m[1] ? ' (' + m[7] + ')' : m[7] : '')
+                }
+            }
+        ])
+    })
+}

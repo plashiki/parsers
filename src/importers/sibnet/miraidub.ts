e0145266055179edb7153edfbc4e492c94b4dde2
teidesu <me@tei.su>
2020-05-31 14:44:40 +0300

    new(parsers): added MiraiDUB Sibnet parser
    Average efficiency: 1.0.
    https://video.sibnet.ru/users/%CE%EA%F1%E0%ED%E0%20%D3%F8%E0%EA%EE%E2%E0

---
+++ src/importers/sibnet/miraidub.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'Оксана Ушакова',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^\[(.+?)\] (.+?) \/ (.+?)(?: - (\d+) серия(?: END)?(?: \(MVO\))?)?$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[2], m[3]],
+                    part: m => m[4] || 1,
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[1]
+                }
+            }
+        ])
+    })
+}

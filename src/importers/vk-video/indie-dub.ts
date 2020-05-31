56c9035c865d9091c2be4851dcfe9ba0d7f393e7
teidesu <me@tei.su>
2020-05-31 16:16:50 +0300

    new(parsers): added IndieDub VK parser
    Average efficiency: 0.8.
    https://vk.com/videos-164696397

---
+++ src/importers/vk-video/indie-dub.ts
@@ -0,0 +1,25 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/vk-video', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/vk-video']({
+        owner: -164696397,
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(?:\[(?:Озвучка )?Indie Dub\] )?(?:\([12]\d{3}г\.\) )?(.+?)(?: (\d+) сезон)?(?: (\d+) серия)?(?: *\/ *| *_ *)(.+?)(?: \d+)?(?: END)?$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [
+                        m[1] + (m[2] ? ' ' + m[2] : ''),
+                        m[4]
+                    ],
+                    part: m => m[3] || '1',
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: 'Indie Dub'
+                }
+            }
+        ])
+    })
+}

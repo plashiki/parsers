1052d7b9aeaad5ae8c6e9dd95acfdd9fbc24ce15
teidesu <me@tei.su>
2020-05-31 16:41:24 +0300

    new(parsers): added animator55 Sibnet parser
    Average efficiency: 0.75, mostly because of old videos with bad naming
    https://video.sibnet.ru/users/animator55

---
+++ src/importers/sibnet/animator55.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'animator55',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(.+?) (?:(\d+)(?: серия)?|S(\d+)E(\d+))?(?: \(?(.+?)(?: Дубляж)?\)?)?$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => m[1] + (m[3] ? ' ' + m[3] : ''),
+                    part: m => m[2] || m[4],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[5]
+                }
+            }
+        ])
+    })
+}

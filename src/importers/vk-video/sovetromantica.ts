f7348d6544b91622cc3a43c8973b14070ec8aad7
teidesu <me@tei.su>
2020-06-07 01:29:15 +0300

    fix(sr): fixed SovetRomantica vk parser
    (new) Average efficiency: ~1.5, mostly because of bad naming in old vids

--- src/importers/vk-video/sovetromantica.ts
+++ src/importers/vk-video/sovetromantica.ts
@@ -16,80 +16,72 @@ export function entry (ctx: ParserContext) {
 
     return ctx.deps['services/vk-video']({
         owner: -33905270,
-        adapter: ctx.deps['common/compose']([
-            async function (video: VkVideo): Promise<Translation[]> {
-                const t = video.title.match(/^\[(озвучка|Дубляж|субтитры) \| (\d+(?: серия)?|фильм|спешл|OVA)(?: END)?\]/)
-                if (!t) {
-                    return []
-                }
-                let enTitle = video.description.match(/^Англоязычное название: (.+?)$/m)
-                let ruTitle = video.description.match(/^Русское название: (.+?)$/m)
-                if (!enTitle && !ruTitle) {
-                    enTitle = video.title.match(/^\[.+?] (.+?) \//)
-                }
+        async adapter (video: VkVideo): Promise<Translation[]> {
+            const t = video.title.match(/^\[(озвучка|Дубляж|субтитры) \| (\d+|фильм|спешл|OVA)(?: серия)?(?:(?: \|)? END)?\]/i)
+            if (!t) {
+                ctx.debug('did not match: %s', video.title)
+                return []
+            }
+            let enTitle = video.description.match(/^Англоязычное название: (.+?)$/m)
+            let ruTitle = video.description.match(/^Русское название: (.+?)$/m)
+            if (!enTitle && !ruTitle) {
+                enTitle = video.title.match(/^\[.+?] (.+?) \//)
+            }
 
-                const titles: string[] = []
-                if (enTitle) {
-                    titles.push(enTitle[1])
-                }
-                if (ruTitle) {
-                    titles.push(ruTitle[1])
-                }
+            const titles: string[] = []
+            if (enTitle) {
+                titles.push(enTitle[1])
+            }
+            if (ruTitle) {
+                titles.push(ruTitle[1])
+            }
 
-                let [, kind, ep] = t
-                kind = kind === 'субтитры' ? 'sub' : 'dub'
-                let episode = (ep === 'фильм' || ep === 'спешл') ? 1 : parseInt(ep.substr(0, ep.length - 6))
-                let authors: string[] = []
-                for (let [, list] of video.description.matchAll(/^(?:Перевод|Редактура|Озвучка): (.+?)$/gm)) {
-                    const names = list.split(/ ?[,&] /g)
-                    names.forEach((name) => {
-                        // remove vk links
-                        const r = name.match(/^\[.+?\|(.+?)]$/)
-                        if (r) {
-                            authors.push(r[1])
-                        } else {
-                            authors.push(name)
-                        }
-                    })
-                }
-
-                // resolve target media
-                const target = await ctx.deps['common/lookup']({
-                    names: titles
+            let [, kind, ep] = t
+            kind = kind === 'субтитры' ? 'sub' : 'dub'
+            let episode = ep.match(/фильм|спешл|ova/i) ? 1 : parseInt(ep)
+            let authors: string[] = []
+            for (let [, list] of video.description.matchAll(/^(?:Перевод|Редактура|Озвучка): (.+?)$/gm)) {
+                const names = list.split(/ ?[,&] /g)
+                names.forEach((name) => {
+                    // remove vk links
+                    const r = name.match(/^\[.+?\|(.+?)]$/)
+                    if (r) {
+                        authors.push(r[1])
+                    } else {
+                        authors.push(name)
+                    }
                 })
-                if (!target) {
-                    ctx.debug('lookup failed')
-                    return []
-                }
+            }
 
-                const translation: Translation = {
-                    hq: video[hqSymbol],
-                    target_id: target.id,
-                    target_type: target.type,
-                    author: authors.length > 0 ? `SovetRomantica (${authors.join(', ')})` : 'SovetRomantica',
-                    kind: kind as TranslationKind,
-                    lang: 'ru',
-                    part: episode,
-                    url: video[urlSymbol]
-                }
-                const ret = [translation]
-
-                let ownPlayer = video.description.match(/https?:\/\/sovetromantica\.com\/anime\/(\d+)\/episode_(\d+-(?:subtitles|dubbed))/)
-                if (ownPlayer) {
-                    let ownPlayerTranslation = ctx.libs.objectUtils.clone(translation)
-                    translation.url = `https://sovetromantica.com/embed/episode_${ownPlayer[1]}_${ownPlayer[2]}`
-                    ret.push(ownPlayerTranslation)
-                }
-
-                return ret
-            },
-            ctx.deps['common/anitomy-adapter']<VkVideo>({
-                target: v => v.title,
-                fallback: {
-                    lang: 'ru',
-                    author: 'SovetRomantica'
-                }
+            // resolve target media
+            const target = await ctx.deps['common/lookup']({
+                names: titles
             })
-        ])
+            if (!target) {
+                ctx.debug('lookup failed')
+                return []
+            }
+
+            const translation: Translation = {
+                hq: video[hqSymbol],
+                target_id: target.id,
+                target_type: target.type,
+                author: authors.length > 0 ? `SovetRomantica (${authors.join(', ')})` : 'SovetRomantica',
+                kind: kind as TranslationKind,
+                lang: 'ru',
+                part: episode,
+                url: video[urlSymbol]
+            }
+            const ret = [translation]
+
+            let ownPlayer = video.description.match(/https?:\/\/(?:www\.)?sovetromantica\.(?:com|moe)\/anime\/(\d+)\/episode_(\d+-(?:subtitles|dubbed))/)
+            if (ownPlayer) {
+                let ownPlayerTranslation = ctx.libs.objectUtils.clone(translation)
+                translation.url = `https://sovetromantica.moe/embed/episode_${ownPlayer[1]}_${ownPlayer[2]}`
+                ret.push(ownPlayerTranslation)
+            }
+
+            return ret
+        }
     })
 }

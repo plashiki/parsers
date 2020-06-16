051f376acd8ececff0daf622f812cda915a620a4
teidesu <me@tei.su>
2020-06-16 17:25:31 +0300

    new(parsers): added AniFresh Sibnet parser
    Average efficiency: 0.97. Again, same regex as for myvi worked O_o
    https://video.sibnet.ru/users/Ksandar87/

---
+++ src/importers/sibnet/anifresh.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'Ksandar87',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(.+?) (\d+)(?: \/ (.+) \2)? (?:Озв\.|Рус\.? озвучка)?\s*(.*)\s*$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[1], m[3]],
+                    part: m => m[2],
+                    kind: 'dub',
+                    lang: 'ru',
+                    author: m => m[4]
+                }
+            }
+        ])
+    })
+}

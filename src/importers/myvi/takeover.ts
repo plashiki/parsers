e48ee9ea75663de649ce21d3a0fad65d2fc761ee
teidesu <me@tei.su>
2020-06-16 16:51:41 +0300

    new(parsers): added TAKEOVER Project Myvi parser
    Average efficiency: 0.87. Surprisingly, same regex as for Sibnet worked o_O
    https://www.myvi.tv/iduqry

---
+++ src/importers/myvi/takeover.ts
@@ -0,0 +1,22 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/myvi', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/myvi']({
+        owner: 'iduqry',
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^\[­?(?:Zakuro Project|TAKEOVER)?(?:_RUS)?(?:\.SUB|\.DUB)?\](?: s\d+)?(?: ep(\d+))?(?: (\d+))?(?: END)? (.+?)(?: |-| - )(?:(\d+)\s*)?(?:END )?(?:[|—\/] (.+?))?(?: [|—\/] (.+?))?(?: s\s*\d+)?(?: ep\s*(\d+))?(?: END)?(?: русск(ая озвучка|ие субтитры))?(?: [\[(]­?(.+?)[\])])?$/i,
+                target: v => v.title,
+                fields: {
+                    target: m => [m[3], m[5], m[6]],
+                    part: m => m[1] || m[2] || m[4] || m[7],
+                    kind: m => m[8]?.match(/субтитры/i) ? 'sub' : 'dub',
+                    lang: 'ru',
+                    author: m => 'TAKEOVER Project' + (m[9] ? ` (${m[9]})` : '')
+                }
+            }
+        ])
+    })
+}

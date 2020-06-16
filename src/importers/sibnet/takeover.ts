4cbfa02902266c8a45e1497a02dbf760ced21e89
teidesu <me@tei.su>
2020-06-16 16:45:49 +0300

    fix(sibnet/takeover): improved regex a bit

--- src/importers/sibnet/takeover.ts
+++ src/importers/sibnet/takeover.ts
@@ -7,14 +7,14 @@ export function entry (ctx: ParserContext) {
         owner: 'Esmeralda Notr-dame',
         adapter: ctx.deps['common/regex-adapter']([
             {
-                regex: /^\[­?(?:Zakuro Project|TAKEOVER)?(?:_RUS)?(?:\.SUB)?\](?: s\d+)?(?: ep(\d+))?(?: END)? (.+?)(?: |-| - )(?:(\d+)\s*)?(?:END )?(?:[|—] (.+?))?(?: [|—] (.+?))?(?: s\s*\d+)?(?: ep\s*(\d+))?(?: END)?(?: русск(ая озвучка|ие субтитры))?(?: [\[(]­?(.+?)[\])])?$/i,
+                regex: /^\[­?(?:Zakuro Project|TAKEOVER)?(?:_RUS)?(?:\.SUB|\.DUB)?\](?: s\d+)?(?: ep(\d+))?(?: (\d+))?(?: END)? (.+?)(?: |-| - )(?:(\d+)\s*)?(?:END )?(?:[|—\/] (.+?))?(?: [|—\/] (.+?))?(?: s\s*\d+)?(?: ep\s*(\d+))?(?: END)?(?: русск(ая озвучка|ие субтитры))?(?: [\[(]­?(.+?)[\])])?$/i,
                 target: v => v.title,
                 fields: {
-                    target: m => [m[2], m[4], m[5]],
-                    part: m => m[1] || m[3] || m[6],
-                    kind: m => m[7]?.match(/субтитры/i) ? 'sub' : 'dub',
+                    target: m => [m[3], m[5], m[6]],
+                    part: m => m[1] || m[2] || m[4] || m[7],
+                    kind: m => m[8]?.match(/субтитры/i) ? 'sub' : 'dub',
                     lang: 'ru',
-                    author: m => 'TAKEOVER Project' + (m[8] ? ` (${m[8]})` : '')
+                    author: m => 'TAKEOVER Project' + (m[9] ? ` (${m[9]})` : '')
                 }
             }
         ])

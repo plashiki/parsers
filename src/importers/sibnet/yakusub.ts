e0eee82d13353230ffa3acd18f3c33cf10fd3ed8
teidesu <me@tei.su>
2020-06-19 15:11:41 +0300

    new(parsers): added YakuSub Sibnet parser
    https://video.sibnet.ru/users/Nika_Elrik/

---
+++ src/importers/sibnet/yakusub.ts
@@ -0,0 +1,24 @@
+import { ParserContext } from '../../../types/ctx'
+
+export const provide = ['services/sibnet', 'common/regex-adapter']
+
+export function entry (ctx: ParserContext) {
+    return ctx.deps['services/sibnet']({
+        owner: 'Nika_Elrik',
+        startFrom: 2185466,
+        adapter: ctx.deps['common/regex-adapter']([
+            {
+                regex: /^(?:\((?:японские|корейские) имена\) )?(.+?)(?: (\d+)(?:\s*\(\d+\))?)?(?: \/ (.+?)(?: \2(?:\s*\(\d+\))?)?)?(?: \(\d+x\d+\))? \[русские субтитры\]/i,
+                skip: v => !!v.title.match(/chico with honeyworks|\s(op|ed)(\d+)?\)|\((op|ed)(\d+)?\s|nowisee|insert song/i),
+                target: v => v.title,
+                fields: {
+                    target: m => [m[1], m[3]],
+                    part: m => m[2] || 1,
+                    kind: 'sub',
+                    lang: 'ru',
+                    author: 'YakuSub Studio'
+                }
+            }
+        ])
+    })
+}
